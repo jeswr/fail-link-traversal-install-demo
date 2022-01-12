e4cff14fa3f4a4d8aeb5ccb2bfba11fe8de700e4
Jesse Wright <jesse.wright@anu.edu.au>
2022-01-13 10:56:27 +1100

    add readme

--- engine-default.js
+++
@@ -1,8 +0,0 @@
-const https___linkedsoftwaredependencies_org_bundles_npm__comunica_bus_init_Bus_Init = new (require('@comunica/core').Bus)({
-  'name': 'https://linkedsoftwaredependencies.org/bundles/npm/@comunica/bus-init/Bus/Init'
-});
-const urn_comunica_my = ({
-  'busInit': https___linkedsoftwaredependencies_org_bundles_npm__comunica_bus_init_Bus_Init
-});
-module.exports = urn_comunica_sparqlinit;
-
